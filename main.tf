6d17ec2fd6d117f17f2355d76617e0b637bf5936
ihtesham <ihtesham1@duck.com>
2022-09-01 19:40:45 +0500

    updated user data

--- main.tf
+++ main.tf
@@ -1,7 +1,24 @@
 resource "aws_instance" "example"{
-    ami = "ami-05fa00d4c63e32376"
+    ami = "ami-052efd3df9dad4825"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, Basit" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+    user_data_replace_on_change = true
     tags = {
       "Name" = "terraform-example"
     }
+}
+
+resource "aws_security_group" "instance" {
+name = "terraform-example-instance"
+ingress {
+from_port = 8080
+to_port = 8080
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
 }
